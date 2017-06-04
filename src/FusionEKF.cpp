5c4a3ff02a6381e2888f777f803ad3cb07eb2afe
Piotr Szczesnowicz <skunkworks@Piotrs-MacBook-Pro.local>
2017-06-04 17:32:50 -0400

    Improve initialization and prevent simultaneous predictions

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -51,7 +51,7 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
     
     ekf_.x_ = VectorXd(4);
 
-    // Initializing the positions of x and y using the first RADAR measurements of rho and phi.
+    // State vector x & y position initialization using the first RADAR measurements of rho and phi.
     if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
       ekf_.x_(0) = measurement_pack.raw_measurements_(0) * cos(measurement_pack.raw_measurements_(1));
       ekf_.x_(1) = measurement_pack.raw_measurements_(0) * sin(measurement_pack.raw_measurements_(1));
@@ -59,7 +59,7 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
       ekf_.x_(3) = 0;
     }
     
-    // Initializing the positions of x and y using the first LASER measurements of x and y.
+    // State vector x & y position initialization using the first LASER measurements of x and y.
     else if (measurement_pack.sensor_type_ == MeasurementPackage::LASER) {
       ekf_.x_(0) = measurement_pack.raw_measurements_(0);
       ekf_.x_(1) = measurement_pack.raw_measurements_(1);
@@ -67,12 +67,22 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
       ekf_.x_(3) = 0;
     }
     
-    // Uncertainty covariance matrix.
+    // Uncertainty covariance matrix initialization.
     ekf_.P_ = MatrixXd(4, 4);
     ekf_.P_ <<  0.1, 0, 0, 0,
                 0, 0.1, 0, 0,
                 0, 0, 1000, 0,
                 0, 0, 0, 1000;
+    
+    // State transition matrix initialization.
+    ekf_.F_ = MatrixXd(4, 4);
+    ekf_.F_ <<  1, 0, 1, 0,
+                0, 1, 0, 1,
+                0, 0, 1, 0,
+                0, 0, 0, 1;
+    
+    // Process noise covariance matrix initialization.
+    ekf_.Q_ = MatrixXd(4, 4);
     
     previous_timestamp_ = measurement_pack.timestamp_;
 
@@ -86,39 +96,37 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
   float dt = (measurement_pack.timestamp_ - previous_timestamp_) / 1000000.0;
   previous_timestamp_ = measurement_pack.timestamp_;
   
-  // State transition matrix.
-  ekf_.F_ = MatrixXd(4, 4);
-  ekf_.F_ <<  1, 0, dt, 0,
-              0, 1, 0, dt,
-              0, 0, 1, 0,
-              0, 0, 0, 1;
-  
-  // Process noise covariance matrix.
-  float dt_squared = pow(dt, 2.0);
-  float dt_cubed = pow(dt, 3.0) / 2.0;
-  float dt_quad = pow(dt, 4.0) / 4.0;
-  
-  ekf_.Q_ = MatrixXd(4, 4);
-  ekf_.Q_ <<  (dt_quad * noise_ax_), 0, (dt_cubed * noise_ax_), 0,
-              0, (dt_quad * noise_ay_), 0, (dt_cubed * noise_ay_),
-              (dt_cubed * noise_ax_), 0, (dt_squared * noise_ax_), 0,
-              0, (dt_cubed * noise_ay_), 0, (dt_squared * noise_ay_);
-  
-  ekf_.Predict();
-  
+  if (dt > 0) {
+    // State transition matrix update.
+    ekf_.F_(0, 2) = dt;
+    ekf_.F_(1, 3) = dt;
+    
+    // Process noise covariance matrix update.
+    float dt_squared = pow(dt, 2.0);
+    float dt_cubed = pow(dt, 3.0) / 2.0;
+    float dt_quad = pow(dt, 4.0) / 4.0;
+    
+    ekf_.Q_ <<  (dt_quad * noise_ax_), 0, (dt_cubed * noise_ax_), 0,
+    0, (dt_quad * noise_ay_), 0, (dt_cubed * noise_ay_),
+    (dt_cubed * noise_ax_), 0, (dt_squared * noise_ax_), 0,
+    0, (dt_cubed * noise_ay_), 0, (dt_squared * noise_ay_);
+    
+    ekf_.Predict();
+  }
+
   // Update step
   
   if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
     ekf_.R_ = R_radar_;
     ekf_.H_ = tools.CalculateJacobian(ekf_.x_);
-    
+      
     ekf_.UpdateEKF(measurement_pack.raw_measurements_);
   }
   
   else if (measurement_pack.sensor_type_ == MeasurementPackage::LASER) {
     ekf_.R_ = R_laser_;
     ekf_.H_ = H_laser_;
-    
+      
     ekf_.Update(measurement_pack.raw_measurements_);
   }
 
